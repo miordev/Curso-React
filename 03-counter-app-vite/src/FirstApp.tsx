3fe9cd84995ecf2926cdf98a5ed88eb9c51a03cf
Miguel Ordoñez <miordev@gmail.com>
2022-06-15 15:26:22 +0100

    Added defaultProps

--- 03-counter-app-vite/src/FirstApp.tsx
+++ 03-counter-app-vite/src/FirstApp.tsx
@@ -1,15 +1,22 @@
 import PropTypes from 'prop-types';
 
-export const FirstApp = ({ title, subtitle }: any) => {
+export const FirstApp = ({ title, subtitle, name }: any) => {
   return (
     <>
-      <h3>{title}</h3>
+      <h1>{title}</h1>
       <p>{subtitle}</p>
+      <p>{name}</p>
     </>
   );
 };
 
 FirstApp.propTypes = {
   title: PropTypes.string.isRequired,
-  subtitle: PropTypes.number.isRequired,
+  subtitle: PropTypes.string.isRequired,
+};
+
+FirstApp.defaultProps = {
+  title: 'There is not title',
+  subtitle: 'There is not subtitle',
+  name: 'Miguel Ordoñez',
 };
--- 03-counter-app-vite/src/main.tsx
+++ 03-counter-app-vite/src/main.tsx
@@ -7,6 +7,6 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <FirstApp title="Hello world!!" subtitle={123} />
+    <FirstApp />
   </React.StrictMode>
 );
